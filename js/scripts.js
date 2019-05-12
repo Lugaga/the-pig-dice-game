2dd41ab9af4275574c91043d520b80fefe2e0791
Lugaga <maureezgaga@gmail.com>
2019-05-13 00:25:28 +0300

    add prototype functions to js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ function ThePigDicePlayer(pigDicePlayerName, rollPigDicePoints, accumulatedRollD
     this.rollPigDicePoints = rollPigDicePoints;
     this.accumulatedRollDicePoints = accumulatedRollDicePoints;
   }
+  ThePigDicePlayer.prototype.roll = function() {
+    var diceNumber = Math.floor(Math.random() * 6) ;
+    if (diceNumber === 1) {
+      this.rollPigDicePoints = 0;
+    } else {
+      this.rollPigDicePoints = this.rollPigDicePoints + diceNumber;
+    };
+    return diceNumber;
+  }
+  ThePigDicePlayer.prototype.score = function() {
+    this.accumulatedRollDicePoints = this.rollPigDicePoints + this.accumulatedRollDicePoints;
+    this.rollPigDicePoints = 0;
+  }
